389b59bf7c52ae248e5f89f4a84bcb4135e600ca
AbDuL RaZaK Mo <r17.abdul@gmail.com>
2020-04-13 21:30:11 +0000

    Bar chart for country

--- src/App.js
+++ src/App.js
@@ -22,17 +22,16 @@ class App extends Component {
         const fetchedData = await fetchData(country);
 
         this.setState({data: fetchedData, country: country});
-        console.log(country)
     }
 
     render() {
-        const {data} = this.state;
+        const {data, country} = this.state;
         return (
             <div className={styles.container}>
             <img className={styles.image} src={image} alt="COVID-19" />
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart />
+                <Chart data={data} country={country} />
             </div>
         );
     }
